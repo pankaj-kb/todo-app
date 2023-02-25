04a038e82d176282ecf44a670d3bdb4eb5d371b9
pankaj-kb <pankajb2601@gmail.com>
2023-02-25 20:51:36 +0530

    adding new functionality with local storage

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,31 +1,53 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function TodoList() {
-  const [todoItems, setTodoItems] = useState(['Write', 'Read', 'Repeat']);
-  const [task, setTask] = useState(()=> "Add New Task")
+  const [todoItems, setTodoItems] = useState([]);
+  const [task, setTask] = useState("Add New Task");
+  const [warning, setWarning] = useState("");
 
   const addTask = () => {
-    setTodoItems([...todoItems,task])
-    setTask("")
-  }
-  return(
+    if (task.trim() === "" || task === "Add New Task") {
+      setWarning("Please enter a valid task.");
+      return;
+    }
+
+    if (todoItems.includes(task)) {
+      setWarning("This task already exists.");
+      return;
+    }
+
+    setTodoItems([...todoItems, task]);
+    setTask("");
+    setWarning("");
+  };
+
+  useEffect(() => {
+    const storedTasks = localStorage.getItem("todoItems");
+    if (storedTasks) {
+      setTodoItems(JSON.parse(storedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todoItems", JSON.stringify(todoItems));
+  }, [todoItems]);
+
+  return (
     <div>
       <ul>
         {todoItems.map((item) => (
           <li key={item}>{item}</li>
         ))}
       </ul>
-      <input type="text" 
+      <input
+        type="text"
         value={task}
         onChange={(e) => setTask(e.target.value)}
       />
-      <button 
-      onClick={addTask}>
-      Add Task
-      </button>
+      <button onClick={addTask}>Add Task</button>
+      {warning && <p>{warning}</p>}
     </div>
-  )
+  );
 }
 
 export default TodoList;
